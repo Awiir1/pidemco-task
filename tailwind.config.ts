18baab35c5d7a53b7a139d43a4efff3061891603
AmirHosein Zare <awir.software@gmail.com>
2025-02-17 15:04:43 +0330

    init project
    configurations and instalations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily:{
+        mainFont: ["DM Sans"]
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
